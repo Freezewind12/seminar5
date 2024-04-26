1efe25354ef65e5cc344555b281b51b534738f0f
Igors Gončarovs <s22goncigor@vea.lv>
2024-04-26 14:23:45 +0300

    Excel addition?

--- src/main/java/lv/venta/model/Product.java
+++ src/main/java/lv/venta/model/Product.java
@@ -1,5 +1,11 @@
 package lv.venta.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,21 +18,25 @@ import lombok.ToString;
 @NoArgsConstructor
 //@RequiredArgsConstructor
 @ToString
+@Table(name = "ProductTable")
+@Entity
 
 public class Product {
 	@Setter(value = AccessLevel.NONE)
+	@Column(name = "ID")
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
+	@Column(name = "TITLE")
 	private String title;
+	@Column(name = "DESCRIPTION")
 	private String description;
+	@Column(name = "PRICE")
 	private float price;
+	@Column(name = "QUANTITY")
 	private int quantity;
 	
-	private static int counter = 1;
-	public void setID() {
-		this.id = counter++;
-	}
 	public Product(String title, String decription, float price, int quantity) {
-		setID();
 		setTitle(title);
 		setDescription(decription);
 		setPrice(price);
