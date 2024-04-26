edc9ef4d269f09737f98ec92a8e68c856983a58b
Igors Gončarovs <s22goncigor@vea.lv>
2024-04-26 16:01:37 +0300

    ...

--- src/main/java/lv/venta/Seminar5Application.java
+++ src/main/java/lv/venta/Seminar5Application.java
@@ -1,7 +1,12 @@
 package lv.venta;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 
 @SpringBootApplication
 public class Seminar5Application {
@@ -9,5 +14,23 @@ public class Seminar5Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Seminar5Application.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner testDatabase(IProductRepo productRepo) {
+		return new CommandLineRunner() {
+			
+			@Override
+			public void run(String... args) throws Exception {
+				Product p1 = new Product("Banana","Yellow",1.99f,3);
+				Product p2 = new Product("Orange","Orange",0.69f,2);
+				Product p3 = new Product("Onion","Brown-ish",1.99f,7);
+				productRepo.save(p1);
+				productRepo.save(p2);
+				productRepo.save(p3);
+				System.out.println("How many: " + productRepo.count());
+				System.out.println("All products: " + productRepo.findAll());
+				System.out.println("Find by id(2): " + productRepo.findById(2).get());
+				
+			}
+		};
+	}
 }
--- src/main/java/lv/venta/service/impl/productServiceImplementation.java
+++ src/main/java/lv/venta/service/impl/productServiceImplementation.java
@@ -2,15 +2,20 @@ package lv.venta.service.impl;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lv.venta.model.Product;
+import lv.venta.repo.IProductRepo;
 import lv.venta.service.iProduct;
 import lv.venta.service.iProductFiltering;
 
 @Service
 public class productServiceImplementation implements iProduct, iProductFiltering{
 
+	@Autowired
+	private IProductRepo productRepo;
+	
 	@Override
 	public void create(String title, String description, float price, int quantity) throws Exception {
 		// TODO Auto-generated method stub
@@ -19,14 +24,17 @@ public class productServiceImplementation implements iProduct, iProductFiltering
 
 	@Override
 	public ArrayList<Product> retirveAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(productRepo.count() == 0)throw new Exception("Product table is empty");
+		ArrayList<Product> result = (ArrayList<Product>) productRepo.findAll();
+		return result;
 	}
 
 	@Override
 	public Product retrieveById(int id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		if(id <= 0) throw new Exception("Id should be positive");
+		if(productRepo.existsById(id)) 
+			return productRepo.findById(id).get();
+		throw new Exception("Product with id (" + id + ") is not in the table");
 	}
 
 	@Override
